e6b0aa6ffe786c042f658e746d4ff5e3d5fcdde0
labyu <labyu2020@gmail.com>
2023-04-16 00:32:24 +0900

    ccc

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import {RouterProvider} from "react-router-dom";
+import routes from "./router/routes";
+import AppHeader from "./components/app-header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const BackgroundContainer = styled.div`
+  background-color: hotpink;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+`;
+
+const SafeArea = styled.div`
+  width: 100%;
+  max-width: 600px;
+  height: 100vh;
+  margin: 0 auto;
+  background-color: #fafafa;
+  overflow: hidden;
+`;
+
+const App = () => (
+  <BackgroundContainer>
+    <SafeArea>
+      <React.StrictMode>
+        <AppHeader />
+        <RouterProvider router={routes} />
+      </React.StrictMode>
+    </SafeArea>
+  </BackgroundContainer>
+);
 
 export default App;
---
+++ src/router/routes.ts
@@ -0,0 +1,12 @@
+import {createBrowserRouter} from "react-router-dom";
+import HomePage from "../pages/home-page";
+
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: HomePage(),
+  }
+]);
+
+export default routes;
