2824717db7cd361dcd7dbe4ba53cea3708253597
김도하 <62235737+kimdoha@users.noreply.github.com>
2023-04-15 16:09:43 +0000

    add axios

--- src/backend/chatgpt.ts
+++ src/backend/chatgpt.ts
@@ -1,12 +1,19 @@
 import { OpenAIApi } from "openai";
 import configuration from "./configuration";
+import axios, { AxiosInstance } from "axios";
 
 class Chatgpt {
 
     openai: OpenAIApi;
+    instance: AxiosInstance;
 
     constructor() {
         this.openai = new OpenAIApi(configuration);
+        this.instance = axios.create({
+            baseURL: 'https://api.openai.com/v1',
+            timeout: 2000,
+            headers: {}
+        });
     }
     
     async listEngines() {
