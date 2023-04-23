ad83cd0da81826f00266ac188d81a9bf4d709d8a
김도하 <62235737+kimdoha@users.noreply.github.com>
2023-04-23 12:27:52 +0000

    환경세팅_2

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
-const express = require('express');
+import cors from 'cors';
+import express from 'express';
+import compression from 'compression';
+import methodOverride from 'method-override';
+// import chatgptRoute from './chatgpt/route';
+
 const app = express();
+const port = 5000;
 
-app.get('/api', (req, res) => {
-    res.json({ message: 'Hello from server!' });
-})
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride());
+app.use(cors());
+// app.use('/', chatgptRoute);
 
-
-app.listen(5000, () => {
-    console.log("Server started on port 5000");
+app.listen(port, () => {
+    console.log(`Server started on port: ${port}`);
 })
